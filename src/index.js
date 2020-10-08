47d78176487bdbcfd5a8e88df6801af57cc89850
Artem Usatyi <badlywaves@gmail.com>
2020-10-08 10:29:11 +0300

    init: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let storeBrackets = [],
+        pairOfBrackets = {};
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        pairOfBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+
+    let arrFromStr = str.split("");
+
+    for (let i = 0; i < arrFromStr.length; i++) {
+        if (arrFromStr[i] === (storeBrackets[storeBrackets.length - 1] || 0)) {
+            storeBrackets.pop();
+        } else {
+            storeBrackets.push(pairOfBrackets[arrFromStr[i]]);
+        }
+    }
+
+    if (storeBrackets.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
